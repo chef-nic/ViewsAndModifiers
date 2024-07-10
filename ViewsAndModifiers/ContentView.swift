4da42e7a44e8545f6f926890ee066a0e96f72b2e
chef-nic <nicholasajohnson@me.com>
2024-07-10 12:06:55 -0700

    custom view modifier

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -9,13 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 10) {
+            Text("First")
+                .titleStyle()
+
+            Text("Second")
+                .titleStyle()
         }
-        .padding()
+    }
+}
+
+struct BlueTitle: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.title)
+            .foregroundStyle(.blue)
+    }
+}
+
+extension View {
+    func titleStyle() -> some View {
+        modifier(BlueTitle())
     }
 }
 
